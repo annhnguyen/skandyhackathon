87e836064037a792bb4735f29026886c011c3822
ghvaleria2606 <g.valeria@wustl.edu>
2025-09-06 01:54:02 -0500

    model training

---
+++ scratch2.py
@@ -0,0 +1,26 @@
+# import tensorflow as tf
+# from tensorflow.keras.models import Sequential, load_model
+# from tensorflow.keras.layers import Dense, Flatten
+# from tensorflow.keras.datasets import mnist #ex. dataset
+# import numpy as np
+# import sys
+# import os
+
+# MODEL_PATH = 'model.h5'
+
+
+
+# '''
+# def build_model(input_shape, num_classes): #defining a simple model -- feedforward
+#     model=Sequential([
+#         Flatten(input_shape=input_shape),
+#         Dense(128, activation='relu'),
+#         Dense(num_classes,activation='softmax')
+#     ])
+#     model.compile(optimizer='adam',
+#                   loss='sparse_categorical_cross entropy',
+#                   metrics=['accuracy'])
+#     return model
+
+# def train_model():
+# '''
--- train.py
+++ train.py
@@ -1,26 +1,64 @@
 import tensorflow as tf
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.datasets import mnist #ex. dataset
-import numpy as np
-import sys
-import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-MODEL_PATH = 'model.h5'
+# classes
+classes = ['cardboard', 'food', 'glass','metal','paper', 'plastic', 'textiles','misc']
+num_classes=len(classes)
 
+# image size
+my_img_size = (128, 128)
+my_batch_size = 32
 
+#training data - normalize data + augmentation
+train_datagen = ImageDataGenerator(
+    rescale=1.0/255,
+    rotation_range=20,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
 
-'''
-def build_model(input_shape, num_classes): #defining a simple model -- feedforward
-    model=Sequential([
-        Flatten(input_shape=input_shape),
-        Dense(128, activation='relu'),
-        Dense(num_classes,activation='softmax')
-    ])
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_cross entropy',
-                  metrics=['accuracy'])
-    return model
+#get training data
+train_data = train_datagen.flow_from_directory(
+    "training_trash_data",
+    target_size=my_img_size,
+    batch_size=my_batch_size,
+    class_mode="categorical"
+)
 
-def train_model():
-'''
+#testing data -normalize
+test_datagen = ImageDataGenerator(rescale=1.0/255)
+
+#get testing data
+test_data = test_datagen.flow_from_directory(
+    "testing_trash_data",
+    target_size=my_img_size,
+    batch_size=my_batch_size,
+    class_mode="categorical"
+)
+
+#building the model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=(128, 128, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(128, (3, 3), activation="relu"),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dense(num_classes, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+# train model
+model.fit(train_data, validation_data=test_data, epochs=10)
+
+# save trained model
+model.save("model.h5")
+
+loss, accuracy = model.evaluate(test_data)
+print("Test accuracy: ", accuracy)
